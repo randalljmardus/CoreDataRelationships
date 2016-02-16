2e3388f67a406f88dd514922a1155fb784e40bc2
Randall Mardus <randalljmardus@gmail.com>
2016-02-16 15:49:22 -0500

    setup view controller

--- CoreDataRelationships/ViewController.swift
+++ CoreDataRelationships/ViewController.swift
@@ -9,20 +9,69 @@
 import UIKit
 import CoreData
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, FetchedResultsDisplayer {
     
+    var tableView = UITableView()
+    var fetchedResultsController: NSFetchedResultsController?
+    var dataSource: FetchedResultsTableDataSource?
     var context: NSManagedObjectContext?
+    var selectedIndexPath: NSIndexPath?
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+    
+    edgesForExtendedLayout = UIRectEdge.None
+    tableView.frame = view.frame
+        
+    tableView.delegate = self
+        tableView.registerClass(UITableViewCell.self, forCellReuseIdentifier: "Cell")
+        
+        view.addSubview(tableView)
+    
+        if let context = context {
+            let request = NSFetchRequest(entityName: "Book")
+            request.sortDescriptors = [NSSortDescriptor(key: "title", ascending: true)]
+            fetchedResultsController = NSFetchedResultsController(fetchRequest: request, managedObjectContext: context, sectionNameKeyPath: nil, cacheName: "BooksTableView")
+            fetchedResultsController?.delegate = FetchedResultsControllerDelegate(displayer: self, tableView: tableView)
+            dataSource = FetchedResultsTableDataSource(displayer: self, fetchedResultsController: fetchedResultsController, cellIdentifier: "Cell")
+            tableView.dataSource = dataSource
+            
+            do {
+                try fetchedResultsController?.performFetch()
+            } catch {
+                print("There was a problem fetching")
+            }
+        }
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    func configureCell(cell: UITableViewCell, object: NSManagedObject) {
+        guard let book = object as? Book else {return}
+        cell.textLabel?.text = book.title
+    }
+    
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        switch segue.identifier! {
+            case "BookInfo":
+                guard let vc = segue.destinationViewController as? BookDetailsViewController else {break}
+                guard let indexPath = selectedIndexPath, book = fetchedResultsController?.objectAtIndexPath(indexPath) as? Book else {return}
+            vc.book = book
+        default:
+            break
+        }
+    }
+}
 
-
+extension ViewController: UITableViewDelegate {
+    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        selectedIndexPath = indexPath
+        performSegueWithIdentifier("BookInfo", sender: nil)
+        tableView.deselectRowAtIndexPath(indexPath, animated: true)
+    }
 }
 
